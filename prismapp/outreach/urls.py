35836428ffca074bdc429c3e535b78212ad069f3
Bamdeb <bamadebupadhya@gmail.com>
2025-09-05 12:00:28 +0530

    New Prism app Python

---
+++ prismapp/outreach/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
+    #path('forgot_password/', views.forgot_password, name='forgot_password'),
+    #path('resetpassword/<str:token>/', views.resetpassword, name='resetpassword'),
+    path('logout/', views.logoutuser, name='logout'),
+    # path('logoutadmin/', views.logoutadmin, name='logoutadmin'),
+    # path('your_score/', views.your_score, name='your_score'),
+    # path('manager_score/', views.manager_score, name='manager_score'),
+    # path('register/', views.register, name='register'),
+    # path('point_by_region/', views.point_by_region, name='point_by_region'),
+    # path('your_current_rank/', views.your_current_rank, name='your_current_rank'),
+    # path('prize/', views.prize, name='prize'),
+    # path('score_history/', views.score_history, name='score_history'),
+    # path('score_history/<int:employee_id>/<int:region_id>/', views.score_history, name='score_history'),
+    # path('fscbyregion/<int:region_id>/', views.fscbyregion, name='fscbyregion'),
+    # path('fscbyregion/', views.fscbyregion, name='fscbyregion'),
+    # path('care_credit/', views.care_credit, name='care_credit'),
+    # path('contact_us/', views.contact_us, name='contact_us'),
+    # path('admin_login/', views.admin_login, name='admin_login'),
+    # path('leaderboard/', views.leaderboard, name='leaderboard'),
+    # path('admin_score_history/<int:employee_id>/', views.admin_score_history, name='admin_score_history'),
+    # path('admin_score_history/', views.admin_score_history, name='admin_score_history'),
+    # path('thank_you/', views.thank_you, name='thank_you'),
+     path('mywork/', views.mywork, name='mywork'),
+    # path('region_manager_score/', views.region_manager_score, name='region_manager_score'),
+
+]
---
+++ prismapp/outreach/views.py
@@ -0,0 +1,94 @@
+
+import requests ,json,re
+import urllib3,os,secrets,string
+#from .utils import send_email
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout,login
+from django.http import HttpResponse
+from datetime import datetime,timedelta
+from django.conf import settings
+from django.contrib import messages
+from django.views.decorators.cache import never_cache, cache_control
+
+# Suppress the InsecureRequestWarning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def home(request):
+    return HttpResponse("Hello, World!")
+
+def login(request):
+    #print('login')
+    if request.method == "POST":
+        username = request.POST.get('userName')
+        password = request.POST.get('password')
+        remember_me = request.POST.get('remember_me')
+        #password = psw.strip().replace(" ", "")
+        #print(remember_me)
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        # Make the API call to 'vegaslogin' endpoint
+        try:
+            response = requests.post(settings.API_URL + "prismAuthentication", json=data)
+            result = response.json()  # Decode the JSON response
+            print(result)
+
+            if response.status_code == 200:
+                user_data = result['data']
+                if isinstance(user_data, list) and len(user_data) > 0:
+                    request.session['user_data'] = user_data[0]
+                    request.session['is_logged_in'] = True
+                    # if remember_me:
+                    #     request.session.set_expiry(1209600)  # 2 weeks (in seconds)
+                    # else:
+                    #     request.session.set_expiry(0)
+
+                    # insert_data = {
+                    #     "employee_id": user_data[0].get('employee_id', None),
+                    #     "action": "LOGIN",
+                    #     "date_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Get current timestamp
+                    # }
+                    # # Prepare the dataList dictionary with the table name
+                    # data_list = {
+                    #     "insertData": insert_data,
+                    #     "table_name": "activity_log"
+                    # }
+                    #
+                    # # Make the API call to insert the data into the activity log table
+                    # requests.post(settings.API_URL + "vegasinsertdataintable", json=data_list)
+
+                    return redirect('/mywork/')
+                    # if request.session['user_data']['employee_role'] == "ZM" or request.session['user_data']['employee_role'] == "ZEM":
+                    #     return redirect('/manager_score/')
+                    # elif request.session['user_data']['employee_role'] == "RM":
+                    #     return redirect('/region_manager_score/')
+
+                else:
+                    return render(request, 'login.html', {'error': 'Invalid username or password.'})
+
+            else:
+                return HttpResponse({"error": "Failed to sign in user", "details": result}, status=400)
+
+        except requests.exceptions.RequestException as e:
+            return HttpResponse({"error": "An error occurred", "details": str(e)}, status=500)
+
+    context = {
+        'path': request.path
+    }
+    # For GET requests, just render the login page
+    return render(request, 'login.html',context)
+
+@cache_control(no_cache=True,must_revalidate=True,no_store=True)
+def mywork(request):
+        print('mywork')
+        if not request.session.get('is_logged_in', False):  # Check session value
+            return render(request, 'login.html')
+        else:
+            return render(request, 'mywork.html')
+
+def logoutuser(request):
+    logout(request)
+    return redirect('/login')
